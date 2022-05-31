43e213e692678fb02166bf69e47ce5bad6ab5e52
Karen Kanana <kananakaren21@gmail.com>
2022-05-31 10:02:23 +0300

    Initial Commit

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,26 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* check_cycle - checks if a singly linked list has a cycle within.
-* @list: singly linked list.
-* Return: 0 is there is no cycle, 1 if there is a cycle.
-*/
-
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *head;
-	listint_t *tail;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (list == NULL)
+	if (!list)
 		return (0);
 
-	head = list;
-	tail = list;
-
-	while (tail != NULL && tail->next != NULL)
+	while (slow && fast && fast->next)
 	{
-		head = head->next;
-		tail = tail->next->next;
-
-		if (head == tail)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
-
 	}
+
 	return (0);
 }
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{:s}{:s}{:s}".format(str, str, str))
-print("{:.9s}".format(str))
+print(3 * str)
+print(str[:9])
