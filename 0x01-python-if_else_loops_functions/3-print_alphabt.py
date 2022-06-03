b027cadd4fe1ccea2e17a747efcf6a46ab89a158
Karen Kanana <kananakaren21@gmail.com>
2022-06-03 14:14:39 +0300

    Initial Commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
 for i in range(97, 123):
-    if i == 113 or i == 101:
+    if i == 113:
         continue
-    print("{:c}".format(i), end="")
+    if i == 101:
+        continue
+    else:
+        print("{:c}".format(i), end='')
