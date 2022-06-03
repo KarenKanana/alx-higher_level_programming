90c8002961b178129cf4098c5265bb198bef679e
Karen Kanana <kananakaren21@gmail.com>
2022-06-03 14:20:08 +0300

    Initial Commit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(100):
-    if i < 99:
-        print("{:02d}".format(i), end=", ")
-    else:
-        print("{:d}".format(i))
+for i in range(0, 99):
+    print("{:02d},".format(i), end=' ')
+print("{:02d}".format(99))
